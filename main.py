7b1115c664531de1e1b7dca95651f681d52255bf
N-Masket <missmasket26@gmail.com>
2025-09-30 15:15:20 +0200

    fibonnacci incomplete

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 def factorial(n):
     """Calculates the factorial of a non-negative integer."""
-    # TODO: Implement the logic
-    pass 
+    
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers")
+    elif n == 0 or n == 1:      
+        return 1    
+    else:
+        return n * factorial(n - 1)
+    
+    
 
 def fibonacci(n):
     """Returns the nth number in the Fibonacci sequence."""
     # TODO: Implement the logic
     return None
 
+
+#Returns a list for the FizzBuzz game up to n.
 def fizzbuzz(n):
-    """
-    Returns a list for the FizzBuzz game up to n.
-    - Multiples of 3 are "Fizz"
-    - Multiples of 5 are "Buzz"
-    - Multiples of both 3 and 5 are "FizzBuzz"
-    """
-    # TODO: Implement the logic
-    return []
+    result = []
+    
+    for i in range(1, n+1):   
+        if i%5==0 and i%3==0:  # Multiples of both 3 and 5 are "FizzBuzz"
+          result.append("FizzBuzz")
+        elif i % 3 == 0:               #- Multiples of 3 are "Fizz"
+         result.append("Fizz")
+        elif i % 5 == 0:
+          result.append("Buzz")    # Multiples of 5 are "Buzz"
+        else:
+          result.append(i)          
+    return result
+    
+   
---
+++ test_main.py
@@ -0,0 +1,53 @@
+import unittest 
+from main import fizzbuzz
+from main import factorial
+
+#fizzbuzz test cases:
+result = [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]
+class TestMethods(unittest.TestCase):
+    
+    #test case 1: input is 3, expected output is "fizz"
+    def test_fizz(self):
+        
+        self.assertEqual(fizzbuzz(3), result[:3])
+    
+    #test case 2: input is 5, expected output is "buzz"
+    def test_Buzz(self):
+        self.assertEqual(fizzbuzz(5), result[:5])
+
+    #test case 3: input is 15, expected output is "fizzbuzz"
+    def  test_fizzbuzz(self):
+        self.assertEqual(fizzbuzz(15), result)
+   
+    #test case 4: input is 7, expected output is "7"
+    def test_fizzNum(self):
+        self.assertEqual(fizzbuzz(7), result[:7])
+        
+
+    #factorial test cases:
+    
+    #test case 1: input is negative, expected output is ValueError
+    def test_negative(self):    
+        with self.assertRaises(ValueError):
+            factorial(-5)
+        
+
+    #test case 2 : input is 0, expected output is 1
+    def test_zero(self):
+        self.assertEqual(factorial(0), 1)
+    
+    #test case 3: input is 1, expected output is 1
+    def test_one(self):     
+        self.assertEqual(factorial(1), 1)
+    
+    #test case 4: input is 5, expected output is 120
+    def test_five(self):                
+        self.assertEqual(factorial(5), 120)
+
+    #test case 5: input is 10, expected output is 3628800
+    def test_ten(self):     
+        self.assertEqual(factorial(10), 3628800)
+    
+
+if __name__ == '__main__':
+    unittest.main()
